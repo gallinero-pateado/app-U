0983f33dd70f3a2a0a382b6d1e3c0bcc0eb9f60e
Byron-acuna <byron.acuna76@gmail.com>
2024-09-27 22:59:57 -0300

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-//Importaciones para Firebase
+//Importaciones para
 import 'package:firebase_core/firebase_core.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
